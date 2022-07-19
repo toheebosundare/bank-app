321f08817f87f79ecb9c1a0c9f46d52760b89b8f
Toheeb Osundare <toheebosundare@gmail.com>
2022-07-19 21:40:31 +0200

    init

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
+
+export default function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='discover'>Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='signup'>Sign up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
